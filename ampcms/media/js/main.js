d3354c23d9b4c723f071e569c6f10da1e47720e1
David Watkins <dwatkinsweb@gmail.com>
2011-09-30 13:49:41 -0700

    move requirejs files to js/lib

--- ampcms/media/js/main.js
+++ ampcms/media/js/main.js
@@ -3,7 +3,7 @@ require.config({
 	paths: {
 		'jquery' : 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min',
 		'ampcms' : '/media/ampcms/js/ampcms',
-		'order' : '/media/ampcms/js/ampcms/order'
+		'order' : '/media/ampcms/js/libs/order'
 	}
 });
 require(['ampcms/core'], function(core) {
