d217a3c4b05633f8126cd593f2419415f1d67002
David Watkins <dwatkinsweb@gmail.com>
2011-10-07 11:31:09 -0700

    Forgot to move sandbox pub/sub out of extension and into main library

--- ampcms/media/js/ampcms/sandbox.js
+++ ampcms/media/js/ampcms/sandbox.js
@@ -10,7 +10,26 @@ define(function() {
 				},
 				push_url : function(url) {
 					pagelet.push_url(url);
-				}
+				},
+				// Event Handling
+				publish : function(event) {
+					pagelet.publish(event);
+				},
+				subscribe : function(events) {
+					pagelet.subscribe(events, module_selector);
+				},
+				unsubscribe : function(events) {
+					pagelet.unsubscribe(events, module_selector);
+				},
+				publish_global : function(event) {
+					pagelet.publish_global(event);
+				},
+				subscribe_global : function(events) {
+					pagelet.subscribe_global(events, module_selector);
+				},
+				unsubscribe_global : function(events) {
+					pagelet.unsubscribe_global(events, module_selector);
+				},
 			};
 			sandbox = pagelet.extend(sandbox, this.extension.create(pagelet, CONTAINER));
 			return sandbox;
--- extensions/jQuery/ampcms/sandbox.js
+++ extensions/jQuery/ampcms/sandbox.js
@@ -3,24 +3,6 @@ define(function() {
 		create : function(pagelet, CONTAINER) {
 			return {
 				// Event Handling
-				publish : function(event) {
-					pagelet.publish(event);
-				},
-				subscribe : function(events) {
-					pagelet.subscribe(events, module_selector);
-				},
-				unsubscribe : function(events) {
-					pagelet.unsubscribe(events, module_selector);
-				},
-				publish_global : function(event) {
-					pagelet.publish_global(event);
-				},
-				subscribe_global : function(events) {
-					pagelet.subscribe_global(events, module_selector);
-				},
-				unsubscribe_global : function(events) {
-					pagelet.unsubscribe_global(events, module_selector);
-				},
 		        bind : function (element, type, fn) {
 		            pagelet.bind(element, type, fn);           
 		        },
