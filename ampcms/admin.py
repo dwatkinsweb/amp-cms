89a75fb3c542e8b5e05827e28968b6a08d059ba0
David Watkins <dwatkinsweb@gmail.com>
2011-10-31 16:09:16 -0700

    Add license agreement to all files

--- ampcms/admin.py
+++ ampcms/admin.py
@@ -1,3 +1,20 @@
+#-------------------------------------------------------------------------------
+# Copyright David Watkins 2011
+# 
+# AMP-CMS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin, GroupAdmin as DjangoGroupAdmin
 from django.contrib.auth.forms import UserChangeForm as DjangoUserChangeForm, UserCreationForm as DjangoUserCreationForm
 from django.contrib.sites.admin import SiteAdmin as DjangoSiteAdmin
