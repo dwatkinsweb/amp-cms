b3c1878b19c3fe84af90b324becd28b69c121ff2
David Watkins <dwatkinsweb@gmail.com>
2011-11-17 08:24:28 -0800

    Add a utility function to be able to build full urls

--- ampcms/utils.py
+++ ampcms/utils.py
@@ -1,3 +1,7 @@
+from ampcms.models import Page
+
+import urllib
+
 try:
     from threading import local
 except ImportError:
@@ -10,3 +14,8 @@ def get_current_request():
 
 def set_current_request(request):
     _thread_locals.request = request
+
+def build_url(site, module_name, page_name, pagelet_urls):
+    page = Page.objects.get(name=page_name, module__name=module_name, module__site=site)
+    pagelet_params = urllib.urlencode(pagelet_urls)
+    return '%s?%s' % (page.get_absolute_url(), pagelet_params)
