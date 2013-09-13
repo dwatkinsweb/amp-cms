27a9beb09a3b5596aaf9c311c9b9d62b1fa7f715
David Watkins <dwatkinsweb@gmail.com>
2013-09-13 11:06:11 -0700

    Abstract out method to get database name by host

--- ampcms/lib/db/routers.py
+++ ampcms/lib/db/routers.py
@@ -1,14 +1,15 @@
 from ampcms.lib.http.request import get_current_request
 from ampcms.conf import settings
+import lib
 
 class HostBasedRouter(object):
     def db_for_read(self, model, **hints):
         request = get_current_request()
         if request is not None:
             host = request.get_host()
-            database_names = settings.AMPCMS_HOST_DATABASES[host]
-            if database_names is not None and database_names in settings.DATABASES:
-                return database_names
-            elif host is not None and host in settings.DATABASES:
+            database = lib.get_database_by_host(host)
+            if database is not None:
+                return database
+            elif host in settings.DATABASES:
                 return host
     db_for_write = db_for_read
