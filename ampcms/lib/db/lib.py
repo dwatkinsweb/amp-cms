0bf8a71236dcd2a3d6cd2ba35ac8d669e1eeee7a
David Watkins <dwatkinsweb@gmail.com>
2013-09-13 11:11:33 -0700

    Forgot to add this file when abstracting out get_database_by_host

---
+++ ampcms/lib/db/lib.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+
+def get_database_by_host(host):
+    database = settings.AMPCMS_HOST_DATABASES.get(host)
+    if database is not None and database in settings.DATABASES:
+        return database
+    else:
+        return None
