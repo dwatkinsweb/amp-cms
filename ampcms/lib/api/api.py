a422eebdcdba6fe9e9607596742537e4932182be
David Watkins <dwatkinsweb@gmail.com>
2013-09-13 11:04:33 -0700

    add active flag to build_url to return None if requested url is not active

--- ampcms/lib/api/api.py
+++ ampcms/lib/api/api.py
@@ -2,13 +2,20 @@ from ampcms.models import Page, Pagelet, AmpCmsSite
 
 import urllib
 
-def build_url(site, module_name, page_name, pagelet_urls=None):
-    page = Page.objects.get(name=page_name, module__name=module_name, module__site=site)
-    if pagelet_urls is not None:
-        pagelet_params = '?'+urllib.urlencode(pagelet_urls)
-    else: 
-        pagelet_params = ''
-    return '%s%s' % (page.get_absolute_url(), pagelet_params)
+def build_url(site, module_name, page_name, pagelet_urls=None, active=None):
+    try:
+        if active is not None:
+            page = Page.objects.get(name=page_name, module__name=module_name, module__site=site, active=True)
+        else:
+            page = Page.objects.get(name=page_name, module__name=module_name, module__site=site)
+    except Page.DoesNotExist:
+        return None
+    else:
+        if pagelet_urls is not None:
+            pagelet_params = '?'+urllib.urlencode(pagelet_urls)
+        else: 
+            pagelet_params = ''
+        return '%s%s' % (page.get_absolute_url(), pagelet_params)
 
 def build_pagelet_url(request, pagelet_url):
     site = AmpCmsSite.objects.get_by_request(request)
