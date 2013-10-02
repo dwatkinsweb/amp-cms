124c1ea2d206654cfc56cb6ad833496586392da6
David Watkins <dwatkinsweb@gmail.com>
2013-10-01 17:16:31 -0700

    make js config file "skinnable"

--- ampcms/context.py
+++ ampcms/context.py
@@ -1,4 +1,16 @@
+import re
 from ampcms.conf import settings
+from ampcms.models import AmpCmsSite
+from django.contrib.staticfiles import finders
+
+JS_PREFIX_RE = re.compile(r'^(/?)((js/)?)(.*)')
 
 def javascript_config(request):
-    return {'AMPCMS_JAVASCRIPT_CONFIG': settings.AMPCMS_JAVASCRIPT_CONFIG}
+    site = AmpCmsSite.objects.get_by_request(request)
+    skin = site.skin
+    if skin:
+        config = JS_PREFIX_RE.sub(r'\1\2%s/\4' % skin, settings.AMPCMS_JAVASCRIPT_CONFIG)
+        found = finders.find(config)
+        if not found:
+            config = settings.AMPCMS_JAVASCRIPT_CONFIG
+    return {'AMPCMS_JAVASCRIPT_CONFIG': config}
